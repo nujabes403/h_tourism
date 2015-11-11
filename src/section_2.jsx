dc5a65ed39b3c03f5bf9a2df6fced88b46445a99
nujbaes403 <nujabes403@gmail.com>
2015-11-11 20:01:34 +0900

    added state to animation 15.11.11

--- src/section_2.jsx
+++ src/section_2.jsx
@@ -1,11 +1,28 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  handleImg:function(e){
+  getInitialState: function() {
+    return {
+      first_img_on:false,
+      second_img_on:false,
+      third_img_on:false
+    };
+  },
+  handle_enterImg:function(e){
     var target_img = e.target.id;
     var t = new TimelineMax();
-    t.to(e.target, .5, {y:'-100'});
+    t.to(e.target, .5, {y:'-70'});
+    this.setState({
+      [e.target.id + '_on'] : true
+    });
+  },
+  handle_leaveImg:function(e){
+    var target_img = e.target.id;
+    var t = new TimelineMax();
     t.to(e.target, .5, {y:'30'});
+    this.setState({
+      [e.target.id + '_on'] : false
+    });
   },
   render:function(){
     return <div className="section_2_main">
@@ -14,15 +31,15 @@ module.exports = React.createClass({
         <h2 className="section_2_jumbotron_h2">Tourism?</h2>
         <div className="row">
           <div className="col-md-4">
-            <img onMouseEnter={this.handleImg} id="first_img" className="section_2_imgs" src="assets/imgs/sales_up.png"/>
+            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="first_img" className="section_2_imgs" src="assets/imgs/sales_up.png"/>
             <h3 className="section_2_jumbotron_h3">성장산업</h3>
             </div>
           <div className="col-md-4">
-            <img onMouseEnter={this.handleImg} id="second_img" className="section_2_imgs" src="assets/imgs/pie_chart.png"/>
+            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="second_img" className="section_2_imgs" src="assets/imgs/pie_chart.png"/>
             <h3 className="section_2_jumbotron_h3">세계경제의 관광분야 기여도</h3>
             </div>
           <div className="col-md-4">
-            <img onMouseEnter={this.handleImg} id="third_img" className="section_2_imgs" src="assets/imgs/employee.png"/>
+            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="third_img" className="section_2_imgs" src="assets/imgs/employee.png"/>
             <h3 className="section_2_jumbotron_h3">전체의 일자리</h3>
             </div>
         </div>
