d383d643167c162d97cec9391ab51b1724d3005b
nujbaes403 <nujabes403@gmail.com>
2015-11-11 19:37:19 +0900

    modified animation code simsimply 15.11.11

--- src/section_2.jsx
+++ src/section_2.jsx
@@ -1,15 +1,11 @@
 var React = require('react');
 
 module.exports = React.createClass({
-  componentDidMount: function() {
-    this.setState({
-      first_img : this.refs.first_img.getDOMNode()
-    });
-  },
-  handleImg:function(){
+  handleImg:function(e){
+    var target_img = e.target.id;
     var t = new TimelineMax();
-    t.to(this.state.first_img, .5, {y:'-100'});
-    t.to(this.state.first_img, .5, {y:'30'});
+    t.to(e.target, .5, {y:'-100'});
+    t.to(e.target, .5, {y:'30'});
   },
   render:function(){
     return <div className="section_2_main">
@@ -18,15 +14,15 @@ module.exports = React.createClass({
         <h2 className="section_2_jumbotron_h2">Tourism?</h2>
         <div className="row">
           <div className="col-md-4">
-            <img onClick={this.handleImg} ref="first_img" className="section_2_imgs" src="assets/imgs/sales_up.png"/>
+            <img onMouseEnter={this.handleImg} id="first_img" className="section_2_imgs" src="assets/imgs/sales_up.png"/>
             <h3 className="section_2_jumbotron_h3">성장산업</h3>
             </div>
           <div className="col-md-4">
-            <img className="section_2_imgs" src="assets/imgs/pie_chart.png"/>
+            <img onMouseEnter={this.handleImg} id="second_img" className="section_2_imgs" src="assets/imgs/pie_chart.png"/>
             <h3 className="section_2_jumbotron_h3">세계경제의 관광분야 기여도</h3>
             </div>
           <div className="col-md-4">
-            <img className="section_2_imgs" src="assets/imgs/employee.png"/>
+            <img onMouseEnter={this.handleImg} id="third_img" className="section_2_imgs" src="assets/imgs/employee.png"/>
             <h3 className="section_2_jumbotron_h3">전체의 일자리</h3>
             </div>
         </div>
