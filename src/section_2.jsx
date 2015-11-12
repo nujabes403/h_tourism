a4096acdf06f69e1f390f805f23a69f4ae2ff105
nujbaes403 <nujabes403@gmail.com>
2015-11-12 09:59:47 +0900

    section2 section4 img animation added 15.11.12

--- src/section_2.jsx
+++ src/section_2.jsx
@@ -3,9 +3,9 @@ var React = require('react');
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      first_img_on:false,
-      second_img_on:false,
-      third_img_on:false
+      sec2_first_img_on:false,
+      sec2_second_img_on:false,
+      sec2_third_img_on:false
     };
   },
   handle_enterImg:function(e){
@@ -31,16 +31,19 @@ module.exports = React.createClass({
         <h2 className="section_2_jumbotron_h2">Tourism?</h2>
         <div className="row">
           <div className="col-md-4">
-            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="first_img" className="section_2_imgs" src="assets/imgs/sales_up.png"/>
+            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="sec2_first_img" className="section_2_imgs" src="assets/imgs/sales_up.png"/>
             <h3 className="section_2_jumbotron_h3">성장산업</h3>
+            {this.state.sec2_first_img_on ? <h5>관광은 고용없는 성장이라는 세계적인 문제에 대한 해결책으로<br/> 국가 이미지 제고, 일자리 창출을 동시에 이룰 수 있는<br/> 성장하는 산업입니다.</h5>:''}
             </div>
           <div className="col-md-4">
-            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="second_img" className="section_2_imgs" src="assets/imgs/pie_chart.png"/>
+            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="sec2_second_img" className="section_2_imgs" src="assets/imgs/pie_chart.png"/>
             <h3 className="section_2_jumbotron_h3">세계경제의 관광분야 기여도</h3>
+            {this.state.sec2_second_img_on ? <h5>세계경제의 관광분야 기여도는 전체 GDP의 9%에 달합니다.</h5>:''}
             </div>
           <div className="col-md-4">
-            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="third_img" className="section_2_imgs" src="assets/imgs/employee.png"/>
+            <img onMouseLeave={this.handle_leaveImg} onMouseEnter={this.handle_enterImg} id="sec2_third_img" className="section_2_imgs" src="assets/imgs/employee.png"/>
             <h3 className="section_2_jumbotron_h3">전체의 일자리</h3>
+            {this.state.sec2_third_img_on ? <h5>전체의 일자리 11개 중 1개는 관광관련 일자리입니다.</h5>:''}
             </div>
         </div>
       </div>
--- src/section_4.jsx
+++ src/section_4.jsx
@@ -1,6 +1,30 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      sec4_first_img_on:false,
+      sec4_second_img_on:false,
+      sec4_third_img_on:false,
+      sec4_fourth_img_on:false
+    };
+  },
+  handle_enterImg:function(e){
+    var target_img = e.target.id;
+    var t = new TimelineMax();
+    t.to(e.target, .5, {y:'-70'});
+    this.setState({
+      [e.target.id + '_on'] : true
+    });
+  },
+  handle_leaveImg:function(e){
+    var target_img = e.target.id;
+    var t = new TimelineMax();
+    t.to(e.target, .5, {y:'20'});
+    this.setState({
+      [e.target.id + '_on'] : false
+    });
+  },
   render:function(){
     return <div className="section_4_main">
       <div className="section_4_jumbotron jumbotron">
@@ -8,20 +32,25 @@ module.exports = React.createClass({
         <h2 className="section_4_jumbotron_h2">Benefits?</h2>
         <div className="row">
           <div className="col-md-3">
-            <img className="section_4_imgs" src="assets/imgs/dahak.png"/>
+            <img onMouseEnter={this.handle_enterImg} onMouseLeave={this.handle_leaveImg} id="sec4_first_img" className="section_4_imgs" src="assets/imgs/dahak.png"/>
             <h3 className="section_4_jumbotron_h3">다학제적 접근</h3>
+            {this.state.sec4_first_img_on ? <h5>관광학은 경영,경제,심리,통계,인문학 등을<br/>포함하여 학습하는 다학제적 접근이 이루어지는 학문으로,이러한 폭 넓은 접근을 바탕으로 <br/>관광이라는 복잡한
+            사회현상에 대한 <br/>이해-분석-해결을 목표로 합니다.</h5>:''}
             </div>
           <div className="col-md-3">
-            <img className="section_4_imgs" src="assets/imgs/dayang.png"/>
+            <img onMouseEnter={this.handle_enterImg} onMouseLeave={this.handle_leaveImg} id="sec4_second_img" className="section_4_imgs" src="assets/imgs/dayang.png"/>
             <h3 className="section_4_jumbotron_h3">다양한 진로</h3>
+            {this.state.sec4_second_img_on ? <h5>국내 관광관련 최고대학인 한양대학교 관광학부에 들어오면 연구소,정부부처,공기업,대기업 등 다양한 진로를 선택할 수 있습니다.</h5>:''}
             </div>
           <div className="col-md-3">
-            <img className="section_4_imgs" src="assets/imgs/taking_money.png"/>
+            <img onMouseEnter={this.handle_enterImg} onMouseLeave={this.handle_leaveImg} id="sec4_third_img" className="section_4_imgs" src="assets/imgs/taking_money.png"/>
             <h3 className="section_4_jumbotron_h3">교비유학 / 외부장학금 <br/>지원</h3>
+            {this.state.sec4_third_img_on ? <h5>관광학부는 세계 여러 대학과 자매결연을 맺고 있어 교비유학이 자유롭고, 외부장학금 지원이 탄탄합니다.</h5>:''}
             </div>
           <div className="col-md-3">
-            <img className="section_4_imgs" src="assets/imgs/networking.png"/>
+            <img onMouseEnter={this.handle_enterImg} onMouseLeave={this.handle_leaveImg} id="sec4_fourth_img" className="section_4_imgs" src="assets/imgs/networking.png"/>
             <h3 className="section_4_jumbotron_h3">업계-학계-공공기관 <br/>한양관광의 탄탄한 네트워킹</h3>
+            {this.state.sec4_fourth_img_on ? <h5>...</h5>:''}
             </div>
         </div>
       </div>
