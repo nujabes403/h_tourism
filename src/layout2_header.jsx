554740cbe818961be9ea998114819b6dc1731bc9
nujbaes403 <nujabes403@gmail.com>
2015-11-14 15:08:13 +0900

    Modified layout2_header through only CSS Material 15.11.14

--- src/layout2_header.jsx
+++ src/layout2_header.jsx
@@ -4,17 +4,34 @@ var Link = ReactRouter.Link;
 module.exports = React.createClass({
   render:function(){
     return <div className="layout2_header">
-      <div className="row">
-        <div className="col-md-4">
-          <h3>Layout2</h3>
-        </div>
-        <div className="col-md-4">
-          header-middle
-        </div>
-        <div className="col-md-4">
-          header-right
-        </div>
-      </div>
+      <header>
+		<nav>
+				<div>
+						<a><span>관광학부 소개</span></a>
+						<div>
+								<a>관광학이란?</a>
+								<a>관광학부 진로</a>
+								<a>관광학부 교수진</a>
+								<a>Link 4</a>
+								<a>Link 5</a>
+								<a>Link 6</a>
+						</div>
+				</div>
+				<div>
+						<a><span>학부생 코너</span></a>
+						<div>
+								<a>무브토크</a>
+								<a>한국문화관광연구원</a>
+								<a>한국관광공사</a>
+								<a>DBPIA</a>
+								<a>RISS</a>
+								<a>자유게시판</a>
+						</div>
+				</div>
+		</nav>
+</header>
+<h1>한양대 관광학부!</h1>
+<p></p>
       </div>
 
   }
