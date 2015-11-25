42841168cbf915b0cc22e33cfa1e19572c0e3cc4
nujbaes403 <nujabes403@gmail.com>
2015-11-26 01:39:17 +0900

    what_tourism page contents modifed 15.11.25

--- src/what_tourism.jsx
+++ src/what_tourism.jsx
@@ -3,11 +3,13 @@ var React = require('react');
 module.exports = React.createClass({
   render:function(){
     return <div className="what_tourism_block">
-      <p>만드는중...ㅠㅠ</p>
+      <div className="jumbotron">
       <h1>관광학이란?</h1>
-      <h4>관광학은 xxx으로 ㅇㅇㅇ 입니다.</h4>
-      <h1>관광학을 배우는 목적은?</h1>
-      <h4>관광학을 배우는 목적은 기본목적으로 </h4>
+      <p>관광이란 체계론적으로 접근하면 <br/><strong>인간</strong>의 여행활동과 <br/>이와 관련된 <strong>사회조직</strong>들의 활동 <br/>그리고 이들을 둘러싸고 있는 <strong>환경</strong>과의
+      상호작용을 통해 이루어지는 모든 사회적 관계이다.</p>
+      <p>관광학은 <span className="alert_custom alert alert-success">응용사회과학</span> 이다.</p>
+      <p></p>
+      </div>
     </div>
   }
 });
