4138f9cbfb013ea8a5748bf24006fda15560a0ca
nujbaes403 <nujabes403@gmail.com>
2015-11-26 23:50:18 +0900

    what_tourism contents addadded

--- src/what_tourism.jsx
+++ src/what_tourism.jsx
@@ -3,13 +3,74 @@ var React = require('react');
 module.exports = React.createClass({
   render:function(){
     return <div className="what_tourism_block">
-      <div className="jumbotron">
+      <div className="jumbotron jum_sec1">
       <h1>관광학이란?</h1>
     <p>관광이란 체계론적으로 접근하면 <br/><strong>인간</strong>의 여행활동과 <br/>이와 관련된 <strong>사회조직</strong>들의 활동 <br/>그리고 이들을 둘러싸고 있는 <strong>환경</strong>과의
       상호작용을 통해 이루어지는<br/> 모든 사회적 관계이다.</p>
-      <p>관광학은 <span className="alert_custom alert alert-success">응용사회과학</span> 이다.</p>
-      <p></p>
-      </div>
+    <br/>
     </div>
+    <div className="jumbotron jum_sec2">
+    <h1>관광학은 응용사회과학이다!</h1>
+    <p>
+        사회과학은 크게 기초사회과학과 응용사회과학으로 구분된다.<br/>
+      기초사회과학은 사회현상에 관한 이론연구를 추구하며,<br/> 관련 응용사회과학분야에 이론적 지식을 제공하는 학문을 말한다.
+      <br/>
+        <strong>응용사회과학</strong>은 기초사회과학 혹은 인접학문으로부터 제공되는 이론적 지식을 활용하여 <span className="underline">사회문제를 해결</span>하고,
+        <br/>
+        동시에 해당분야에 필요한 <span className="underline">전문 인력을 양성</span>하는데 목적을 둔다.
+        <br/>관광학이 이러한 응용사회과학이다.
+      </p>
+
+    </div>
+    <div className="jumbotron jum_sec3">
+      <br/>
+      <h1>관광학 연구의 목적은 </h1>
+      <p><strong>1.기본목적</strong>
+      <br/>
+      <span className="alert_custom1 alert alert-warning">관광학 연구의 기본적인 목적은 '관광지식의 제공'이다.</span>
+      <br/>
+      관광학은 관광에 관한 지식(knowledge of tourism)을 제공하는 데 목적이 있는 동시에,<br/> 관광에 필요한 지식(knowledge for tourism)을
+      제공하는 데 목적이 있다.
+      <br/>
+      관광에 관한 지식은 관광현상을 이해하기 위한 기본지식을 말한다.
+      <br/>기본지식은 관광자, 관광조직, 관광환경 등의 개념 및 관계에 대한 일반지식이다.
+      <br/>반면에 관광에 필요한 지식은 관광문제를 해결하기 위한 응용지식을 말한다.
+      <br/>
+      응용지식은 관광경영, 관광개발, 관광마케팅, 관광정책, 관광미디어커뮤니케이션 등 관광문제 해결을
+      위한 전문지식이다.
+      <br/>
+      <br/>
+      <strong>2.중간목적</strong>
+      <br/>
+      <span className="alert_custom2 alert alert-danger">관광학 연구의 중간 목적은 '사회문제의 해결'이다.</span>
+      <br/>관광은 복합적인 사회현상으로 인간의 여행활동, 사회조직, 그리고 환경을 포함한다.
+      <br/>이들 간의 관계에서 발생하는 사회문제를 해결하기 위하여 관광학 지식이 필요하다.
+        <br/>예컨대, 관광자의 여행활동에 장애가 되는 요인들을 제거하는 문제,
+          <br/>관광자가 필요로 하는 관광시설이나 서비스를 공급하는 문제,
+            <br/>보다 많은 관광자를 유치하는 문제,
+              <br/>관광사업조직들의 경쟁력에 관한 문제, 국제관광 활성화를 위한 국가 간의 협력 문제 등
+              <br/> 다양한 관광과 관련된 사회문제들이 있다.
+                <br/>이러한 문제들을 해결함으로써 관광학은 '관광을 통한 경제발전','관광을 통한 문화교류','관광을 통한 인류평화 증진','관광을 통한 환경보전',
+                  '관광을 통한 사회복지'등을 도모한다.
+                  <br/>
+                  <br/>
+        <strong>3.궁극적 목적</strong>
+        <br/>
+        <span className="alert_custom3 alert alert-success">관광학 연구의 궁극적 목적은 '인간의 행복 실현'이다.</span>
+        <br/>
+        관광학 연구의 목적은 중간 목적에서 제시한 관광과 관련된 사회문제 해결에만 그치지 않는다.
+        <br/>
+        관광학 연구는 관광이 곧 인간 생활에 필요한 기본적인 활동이라는 점에 주목한다.
+        <br/>
+        그런 의미에서 관광학 연구는 궁극적으로는 관광을 통한 인간의 행복 실현에 목표를 둔다.
+        <br/>행복은 인간의 욕구가 충족된 상태를 말한다.
+        인간의 욕구에는 생리적 욕구, 안전 욕구, 사회적 관계 욕구,
+        자기존중 욕구, 자기실현 욕구 등 다양한 욕구가 있다.
+        <br/>관광을 통해 이러한 욕구가 충족될 수 있도록 필요한 지식을
+        제공하는 것이 관광학의 목적이다.
+      </p>
+      </div>
+      </div>
+
   }
 });
